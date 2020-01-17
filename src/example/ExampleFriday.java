c94885f81192587c3d60d445fb773a8ea855c427
kasmaniz <kasmaniz@sheridancollege.ca>
2020-01-17 16:22:43 -0500

    by me

---
+++ src/example/ExampleFriday.java
@@ -0,0 +1,21 @@
+/*
+ * Zohaib Kasmani
+ * 991545984
+ * SYST10199-Web Programming
+ */
+package example;
+
+/**
+ *
+ * @author Taby zeri
+ */
+public class ExampleFriday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Example");
+    }
+    
+}
